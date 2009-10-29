5c8670510b16ebf1db221cadf6d266828767a6f3
Benjamin Kellermann <Benjamin.Kellermann@gmx.de>
2009-10-29 09:26:22 +0100

    better wording

--- config_sample.rb
+++ config_sample.rb
@@ -6,7 +6,7 @@ SITEURL = "http://#{$cgi.server_name}#{$cgi.script_name.gsub(/[^\/]*$/,"")}"
 
 # add this htmlcode to the startpage
 NOTICE = <<NOTICE
-<fieldset><legend>Sandbox</legend>
+<fieldset><legend>Examples</legend>
 	If you want to play around with the Tool, you may want to take a look at these two Example Polls:<br />
 	<a href='EventScheduleExample'>Event Schedule Poll</a><br />
 	<a href='NormalExample'>Normal Poll</a>	
