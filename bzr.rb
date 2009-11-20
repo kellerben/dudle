ec2cecc1fbf157cbed88e5bcbc939bed783abff2
Benjamin Kellermann <Benjamin.Kellermann@gmx.de>
2009-11-20 11:12:19 +0100

    License changed

--- bzr.rb
+++ bzr.rb
@@ -1,8 +1,21 @@
-################################
-# Author:  Benjamin Kellermann #
-# License: CC-by-sa 3.0        #
-#          see License         #
-################################
+############################################################################
+# Copyright 2009 Benjamin Kellermann                                       #
+#                                                                          #
+# This file is part of dudle.                                              #
+#                                                                          #
+# Dudle is free software: you can redistribute it and/or modify it under   #
+# the terms of the GNU Affero General Public License as published by       #
+# the Free Software Foundation, either version 3 of the License, or        #
+# (at your option) any later version.                                      #
+#                                                                          #
+# Dudle is distributed in the hope that it will be useful, but WITHOUT ANY #
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or        #
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public     #
+# License for more details.                                                #
+#                                                                          #
+# You should have received a copy of the GNU Affero General Public License #
+# along with dudle.  If not, see <http://www.gnu.org/licenses/>.           #
+############################################################################
 
 BZRCMD="export LC_ALL=de_DE.UTF-8; bzr"
 require "time"
