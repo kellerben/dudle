ad3a18244f645555c57f76a14debeeed5e607775
Benjamin Kellermann <Benjamin.Kellermann@gmx.de>
2010-03-09 19:33:57 +0100

    Swedish localization added (thanks to Erica)

--- dudle.rb
+++ dudle.rb
@@ -205,7 +205,8 @@ READY
 		@html << "<div id='languageChooser'>"
 		lang = [
 			["en", "English"],
-			["de", "Deutsch"]
+			["de", "Deutsch"],
+			["sv", "Svenska"]
 			]
 		lang.each{|short,long|
 			@html << "<a href='?lang=#{short}'>" unless short == GetText.locale.language
