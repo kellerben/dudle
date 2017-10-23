765bc27ae016ca204550db2d9961fa835877519b
Benjamin Kellermann <Benjamin.Kellermann@gmx.de>
2017-10-23 22:36:23 +0200

    add possibility to revert poll (closes #10, #79)

---
+++ advanced.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+
+############################################################################
+# Copyright 2017 Benjamin Kellermann                                       #
+#                                                                          #
+# This file is part of dudle.                                              #
+#                                                                          #
+# Dudle is free software: you can redistribute it and/or modify it under   #
+# the terms of the GNU Affero General Public License as published by       #
+# the Free Software Foundation, either version 3 of the License, or        #
+# (at your option) any later version.                                      #
+#                                                                          #
+# Dudle is distributed in the hope that it will be useful, but WITHOUT ANY #
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or        #
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public     #
+# License for more details.                                                #
+#                                                                          #
+# You should have received a copy of the GNU Affero General Public License #
+# along with dudle.  If not, see <http://www.gnu.org/licenses/>.           #
+############################################################################
+
+if __FILE__ == $0
+load "../dudle.rb"
+
+hintstr = ""
+if $cgi.include?("undo_revision") && $cgi["undo_revision"].to_i < VCS.revno
+	undorevision = $cgi["undo_revision"].to_i
+	$d = Dudle.new(:revision => undorevision)
+	comment = $cgi.include?("redo") ? "Redo changes" : "Reverted Poll"
+	$d.table.store("#{comment} to version #{undorevision}")
+	$d << "<h2>" + _("Revert Poll") + "</h2>"
+	$d <<  _("Poll was reverted to Version %{version}!" % {:version => undorevision})
+else
+	$d = Dudle.new
+	$d << "<h2>" + _("Revert Poll") + "</h2>"
+	$d << "<form method='POST'><div>"
+	$d <<  _("Revert Poll to version (see History tab for revision numbers): ")
+	$d << "<input type='text' name='undo_revision' />"
+	$d << "<input type='submit' value='#{_('Revert')}' />"
+	$d << "</div>"
+end
+
+$d.out
+end
